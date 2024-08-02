874950538fe485649a4c43cbc458b6fee8a9c70f
Cole <coltonflather@gmail.com>
2024-08-02 10:42:45 -0400

    Removed keys

--- sendEmail.js
+++ sendEmail.js
@@ -7,12 +7,10 @@ const TOKEN_PATH = path.join(__dirname, 'token.json');
 const token = JSON.parse(fs.readFileSync(TOKEN_PATH, 'utf8'));
 
 // Your credentials from the Google Cloud Console
-const CLIENT_ID = '993851712443-03bi43b2tlq3pel0gn19fo3jiovuut58.apps.googleusercontent.com';
-const CLIENT_SECRET = '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';
-const REDIRECT_URI = 'urn:ietf:wg:oauth:2.0:oob';
+
 
 // Create OAuth2 client
-const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
+const oAuth2Client = new google.auth.OAuth2(process.env.CLIENT_ID, process.env.CLIENT_SECRET, process.env.REDIRECT_URI);
 oAuth2Client.setCredentials(token);
 
 // Function to send an email
