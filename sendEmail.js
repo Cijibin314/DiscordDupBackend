e793a89b238a4cfcf605bd77346e309c48ba8c58
Cole <coltonflather@gmail.com>
2024-08-02 21:20:14 -0400

    Fixed sendEmail not being defined at the right time

--- sendEmail.js
+++ sendEmail.js
@@ -1,63 +1,63 @@
 require("dotenv").config();
 const { google } = require('googleapis');
-const fs = require('fs');
+const fs = require('fs').promises; // Use promises API for fs
 
 // Initialize the OAuth2 client
 const oAuth2Client = new google.auth.OAuth2(
   process.env.CLIENT_ID,
   process.env.CLIENT_SECRET,
-  'urn:ietf:wg:oauth:2.0:oob' // redirect uri
+  'urn:ietf:wg:oauth:2.0:oob'
 );
-let sendEmail;
-// Check if the token is loaded correctly
-fs.readFile('token.json', (err, token) => {
-  if (err) {
-    return console.error('Error loading token:', err);
-  }
-  
-  const tokenData = JSON.parse(token);
+
+
+// Function to load the token and initialize sendEmail
+let token;
+let tokenData;
+async function init(){
+  token = await fs.readFile('token.json', 'utf8');
+  tokenData = JSON.parse(token);
   console.log('Token loaded:', tokenData);
   oAuth2Client.setCredentials(tokenData);
+  console.log("Done")
+}
+// Define sendEmail function
+init()
+async function sendEmail(emailAddress, username){
+  const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
 
-  // Function to send an email
-  sendEmail = async (emailAddress, username) =>{
-    const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+  const emailLines = [
+    'From: "Sender Name" <discorddupemail82194835y726@gmail.com>',
+    `To: ${emailAddress}`,
+    'Subject: Password Reset of Discord Duplicate',
+    '',
+    `Link to your password reset: https://cijibin314.github.io/DiscordDupPasswordReset?username=${username}`
+  ];
 
-    const emailLines = [
-      'From: "Sender Name" <discorddupemail82194835y726@gmail.com>',
-      `To: ${emailAddress}`,
-      'Subject: Password Reset of Discord Duplicate',
-      '',
-      `Link to your password reset: https://cijibin314.github.io/DiscordDupPasswordReset?username=${username}`
-    ];
+  const email = emailLines.join('\n');
 
-    const email = emailLines.join('\n');
+  const encodedMessage = Buffer.from(email)
+    .toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
 
-    const encodedMessage = Buffer.from(email)
-      .toString('base64')
-      .replace(/\+/g, '-')
-      .replace(/\//g, '_')
-      .replace(/=+$/, '');
-
-    try {
-      const res = await gmail.users.messages.send({
-        userId: 'me',
-        requestBody: {
-          raw: encodedMessage,
-        },
-      });
-      console.log('Email sent:', res.data);
-      return true;
-    } catch (error) {
-      console.error('Error sending email:', error);
-      return "Error sending email";
-    }
+  try {
+    const res = await gmail.users.messages.send({
+      userId: 'me',
+      requestBody: {
+        raw: encodedMessage,
+      },
+    });
+    console.log('Email sent:', res.data);
+    return true;
+  } catch (error) {
+    console.error('Error sending email:', error);
+    return "Error sending email";
   }
+};
 
-  // Test sending an email
-});
-setTimeout(()=>{
-  module.exports = {
-    sendEmail
-  };}
-, 1000)
+// Initialize the client and export sendEmail when ready
+
+module.exports = {
+  sendEmail
+};
